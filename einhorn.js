dd1dec079eabbd309cde2fbd1e2ffeb4281a77bc
Christin <christin-thoma@t-online.de>
2020-04-05 16:50:05 -0600

    Updated version 4.50 pm on a snowy SUnday

--- einhorn.js
+++ einhorn.js
@@ -3,4 +3,5 @@ var a= 1+2
 console.log(a)
 let b= function(a=> a+5)
 console.log(b, a)
-
+console.log("These are some more commits")
+console.log("We want to experience a lot of commitments a computer can make")
